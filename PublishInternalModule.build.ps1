5f820db4971605c6c8b10ae00f659bb890c0f04e
psymonn <40018954+psymonn@users.noreply.github.com>
2018-09-12 17:07:59 +1000

    Update PublishInternalModule.build.ps1

--- PublishInternalModule.build.ps1
+++ PublishInternalModule.build.ps1
@@ -11,41 +11,41 @@
 $config = Get-Content -Path .\config.json | ConvertFrom-Json
 
 # Synopsis: Run/Publish Tests and Fail Build on Error
-
-task Test BeforeTest, RunTests, AfterTest
+task Test BeforeTest, RunTests, AfterTest, Publish
 task Dependencies SaveDependency, ImportDependency
 
-
-
 # Synopsis: Run full Pipleline.
-
 #task . SaveDependency, ImportDependency, Clean, Test, Publish
-task . Dependencies, Clean, Test, Publish
+task . initPreference, UpdateBuildManifest, Dependencies, Clean, Test, Publish
 #task . InstallDependencies, Clean, Analyse, Test, Archive
 
+#init override automatic variable
+task initPreference {
 
+    Write-Host "ErrorActionPreference: $ErrorActionPreference"
+    $script:ErrorActionPreference = 'Continue'
+    $script:PsymonPeferenceVariable = 'Who am I?'
 
-# Synopsis: Install Build Dependencies
-
-task SaveDependency {
-
-    write-host 'Save dependencies'
-    #script:$config = Get-Content -Path .\config.json | ConvertFrom-Json
-    Save-Dependency($config)
-
-}
-
-
-task ImportDependency {
-
-    write-host 'Import dependencies'
-    Import-Dependency
+    #InitPreferenceSingle
+    
+    InitPreferenceMultiple
 
 }
 
+task UpdateBuildManifest {
+
+    $moduleInfo = @{
+
+        ModuleName = $ModuleName
+
+        BuildNumber = $BuildNumber
+
+    }
+
+    Update-BuildManifest @moduleInfo
+}
 
 # Synopsis: Clean Artifacts Directory
-
 task Clean BeforeClean, {
 
     if(Test-Path -Path $Artifacts)
@@ -67,20 +67,55 @@ task Clean BeforeClean, {
 }, AfterClean
 
 
+# Synopsis: Install Build Dependencies
+task SaveDependency {
+
+    write-host 'Save dependencies'
+    #script:$config = Get-Content -Path .\config.json | ConvertFrom-Json
+
+    $moduleInfo = @{
+
+        SaveRepoName    = $Settings.SaveRepoName
+        Artifacts       = $Artifacts
+        config          = $config
+
+    }
+
+    #Save-Dependency($config)
+    Save-Dependency @moduleInfo -Verbose
+
+}
+
+
+task ImportDependency {
+
+    write-host 'Import dependencies'
+    Import-Dependency
+
+}
+
+
 # Synopsis: Test the project with Pester. Publish Test and Coverage Reports
 
 task RunTests {
-    #$ErrorActionPreference = 'Continue'
-    #initPreference
+
     write-host 'Test dependency'
     Test-Dependency($config)
 
 }
 
 # Synopsis: Publish to SMB File Share
-
 task Publish BeforePublish, {
 
-    Publish-Depedency
+    $moduleInfo = @{
+
+        PubRepoName = $Settings.PublishRepoName
+
+        RepoApiKey = $Settings.ApiKey
+
+        Artifacts = $Artifacts
+    }
+
+    Publish-Depedency @moduleInfo -Verbose
 
 }, AfterPublish
